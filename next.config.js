e3fdf545fdb7b8c47641b1e08ab31a3667e7be03
Ghostly Builder <ghostly@example.com>
2026-02-03 21:11:41 -0400

    Fix: Transpile 3D libraries to prevent white screen

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    transpilePackages: ['three', 'react-globe.gl', 'react-leaflet']
+};
 
 module.exports = nextConfig;
